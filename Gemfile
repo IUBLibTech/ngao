31d9f5982dfee19e42a31b7d202cb2376be1bfdc
Daniel Lim <daniel@notch8.com>
2020-03-23 15:33:29 -0700

    added capybara and imported settings from arclight

--- Gemfile
+++ Gemfile
@@ -39,6 +39,16 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry'
+
+  gem 'rspec'
+  gem 'rspec-rails'
+
+  # TODO: set up coveralls
+  gem 'coveralls', require: false
+  gem 'simplecov', require: false
+
+  gem 'database_cleaner'
 end
 
 group :development do
---
+++ spec/rails_helper.rb
@@ -0,0 +1,129 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    # Coveralls::SimpleCov::Formatter
+  ]
+)
+SimpleCov.start('rails')
+
+require File.expand_path('../config/environment', __dir__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'database_cleaner'
+require 'factory_bot'
+
+require 'selenium-webdriver'
+require 'webdrivers'
+# Add additional requires below this line. Rails is not loaded until this point!
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+#
+# The following line is provided for convenience purposes. It has the downside
+# of increasing the boot-up time by auto-requiring all files in the support
+# directory. Alternatively, in the individual `*_spec.rb` files, manually
+# require only the support files necessary.
+#
+# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+# Capybara configurations from arclight
+Capybara.javascript_driver = :headless_chrome
+
+Capybara.register_driver :headless_chrome do |app|
+  Capybara::Selenium::Driver.load_selenium
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.args << '--headless'
+    opts.args << '--disable-gpu'
+    opts.args << '--no-sandbox'
+    opts.args << '--window-size=1280,1696'
+  end
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
+Capybara.default_max_wait_time = 15 # our ajax responses are sometimes slow
+
+Capybara.enable_aria_label = true
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your specs with their type, e.g.:
+  #
+  #     RSpec.describe UsersController, :type => :controller do
+  #       # ...
+  #     end
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
+end
+
+# Provide a custom matcher that makes it easier to deal with pretty-printed
+# XML
+
+require 'rspec/expectations'
+
+def condense_whitespace(str)
+  str.squish.strip.gsub(/>[\n\s]+</, '><')
+end
+
+def equal_modulo_whitespace(string1, string2)
+  condense_whitespace(string1) == condense_whitespace(string2)
+end
+
+RSpec::Matchers.define :eq_ignoring_whitespace do |expected|
+  match do |actual|
+    condense_whitespace(expected) == condense_whitespace(actual)
+  end
+end
+
+RSpec::Matchers.define :include_ignoring_whitespace do |expected|
+  ex = condense_whitespace(expected)
+  match do |actual|
+    actual.any? { |act| condense_whitespace(act) == ex }
+  end
+end
+
+RSpec::Matchers.define :equal_array_ignoring_whitespace do |expected|
+  match do |actual|
+    actual.map { |act| condense_whitespace(act) } == expected.map { |ex| condense_whitespace(ex) }
+  end
+end
