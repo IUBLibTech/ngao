5602e29e27a4fde735842a0737e032683a26d8c7
Daniel Lim <daniel@notch8.com>
2020-03-23 16:39:46 -0700

    copied sample feature spec to test capybara

---
+++ spec/features/search_breadcrumb_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Search Breadcrumb', type: :feature do
+  context 'on regular search results' do
+    it do
+      visit search_catalog_path q: 'a brief', search_field: 'all_fields'
+      within '.al-search-breadcrumb' do
+        expect(page).to have_css 'a', text: 'Home'
+        expect(page).to have_content 'Search results'
+      end
+    end
+
+    it do
+      visit search_catalog_path f: { level_sim: ['Collection'] }, search_field: 'all_fields'
+      within '.al-search-breadcrumb' do
+        expect(page).to have_css 'a', text: 'Home'
+        expect(page).to have_content 'Collections'
+      end
+    end
+  end
+end
