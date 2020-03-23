d2a0dcf21bfc46d773595729ffd18203d21253ff
Daniel Lim <daniel@notch8.com>
2020-03-23 16:22:47 -0700

    added sample view spec

---
+++ spec/views/shared/_header_navbar.html.erb_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+RSpec.describe 'shared/_header_navbar.html.erb', type: :view do
+  context 'header' do
+    before do
+      allow(view).to receive(:render_search_bar).and_return('')
+      render
+    end
+    it 'renders iu branding' do
+      expect(rendered).to render_template('layouts/_branding_bar')
+    end
+  end
+end
