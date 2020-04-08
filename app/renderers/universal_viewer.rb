634d0619f1a00e21eb62a4464846f0f0ef815d39
Lea Ann Bradford <leaann@notch8.com>
2020-04-08 09:43:23 -0700

    use hard coded uv

--- app/renderers/universal_viewer.rb
+++ app/renderers/universal_viewer.rb
@@ -4,18 +4,6 @@ class UniversalViewer
     @document = document
   end
 
-  def to_partial_path
-    'viewers/_universal_viewer'
-  end
-
-  def url
-    "#{base_url}#?manifest=#{manifest_url}"
-  end
-
-  def base_url
-    Plantain.config[:external_universal_viewer_url]
-  end
-
   def manifest_url
     @document.digital_objects.first.href
   end
