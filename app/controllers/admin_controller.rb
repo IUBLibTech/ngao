cae8c592a1fbb4787d115ca151224071fdc33565
Lea Ann Bradford <leaann@notch8.com>
2020-04-26 17:23:29 -0700

    add delete user and update role

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -21,4 +21,22 @@ class AdminController < ApplicationController
     args = { ead: file, repository: repository }
     EadProcessor.delay.index_single_ead(args)
   end
+
+  def delete_user
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_path, notice: 'User has been deleted.' if @user.destroy
+  end
+
+  def update_user_role
+    @user = User.find(params[:id])
+    @user.role == 'manager' ? @user.role = 'admin' : @user.role = 'manager'
+    @user.save
+
+    if @user.save
+      redirect_to admin_path, notice: 'User role has been updated'
+    else
+      redirect_to admin_path, notice: 'User was not updated'
+    end
+  end
 end
