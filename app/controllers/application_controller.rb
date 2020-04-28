a4c00cb362edabf3764bcf2244a476c8a0650d8b
Daniel Lim <daniel@notch8.com>
2020-04-28 08:54:39 -0700

    added redirect after accepting and sending invitation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || admin_path
   end
+
+  def after_accept_path_for(resource)
+    stored_location_for(resource) || admin_path
+  end
+
+  def after_invite_path_for(resource)
+    stored_location_for(resource) || admin_path
+  end
 end
