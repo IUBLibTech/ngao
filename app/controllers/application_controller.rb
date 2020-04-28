c664cb07d8093d164e1b9060fafd928bfd4aa3c2
Daniel Lim <daniel@notch8.com>
2020-04-28 08:45:40 -0700

    redirect to admin dashboard after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
   include Blacklight::LocalePicker::Concern
   layout :determine_layout if respond_to? :layout
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || admin_path
+  end
 end
