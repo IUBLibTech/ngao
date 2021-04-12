e6ff70af01b9f6e54bad08044082fe5c38690ca3
Mason Ballengee <masaball@iu.edu>
2021-04-12 09:52:36 -0400

    Fix: large lists failing to index
    A handful of finding aids in the Lilly Repo have been failing to index
    because of notes containing lists that exceed the byte limit that
    is set for _ssm (string) type fields.
    
    Duke and other institutions have encountered similar issues. Their
    solution was to change the _ssm note fields to _tesim (text).
    
    This fix changes controller and config parameters so that
    scopecontent notes will be indexed as _tesim.

--- lib/ngao-arclight/traject/ead2_config.rb
+++ lib/ngao-arclight/traject/ead2_config.rb
@@ -246,12 +246,14 @@ end
 
 SEARCHABLE_NOTES_FIELDS.map do |selector|
   to_field "#{selector}_ssm", extract_xpath("/ead/archdesc/#{selector}/*[local-name()!='head']", to_text: false)
+  to_field "scopecontent_tesim", extract_xpath("/ead/archdesc/scopecontent/*[local-name()!='head']", to_text: false)
   to_field "#{selector}_heading_ssm", extract_xpath("/ead/archdesc/#{selector}/head") unless selector == 'prefercite'
   to_field "#{selector}_teim", extract_xpath("/ead/archdesc/#{selector}/*[local-name()!='head']")
 end
 
 DID_SEARCHABLE_NOTES_FIELDS.map do |selector|
   to_field "#{selector}_ssm", extract_xpath("/ead/archdesc/did/#{selector}", to_text: false)
+  to_field "scopecontent_tesim", extract_xpath("/ead/archdesc/did/scopecontent", to_text: false)
 end
 
 NAME_ELEMENTS.map do |selector|
@@ -562,11 +564,13 @@ compose 'components', ->(record, accumulator, _context) { accumulator.concat rec
 
   SEARCHABLE_NOTES_FIELDS.map do |selector|
     to_field "#{selector}_ssm", extract_xpath("./#{selector}/*[local-name()!='head']", to_text: false)
+    to_field "scopecontent_tesim", extract_xpath("./scopecontent/*[local-name()!='head']", to_text: false)
     to_field "#{selector}_heading_ssm", extract_xpath("./#{selector}/head")
     to_field "#{selector}_teim", extract_xpath("./#{selector}/*[local-name()!='head']")
   end
   DID_SEARCHABLE_NOTES_FIELDS.map do |selector|
     to_field "#{selector}_ssm", extract_xpath("./did/#{selector}", to_text: false)
+    to_field "scopecontent_tesim", extract_xpath("./did/scopecontent", to_text: false)
   end
   to_field 'did_note_ssm', extract_xpath('./did/note')
 end
