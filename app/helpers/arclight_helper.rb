7eae22a082ce8179d22b52402b25c580344648ed
McClain Looney <mcclain@curationexperts.com>
2022-06-15 13:58:42 -0500

    Adds collection id to search results

--- app/helpers/arclight_helper.rb
+++ app/helpers/arclight_helper.rb
@@ -24,7 +24,11 @@ module ArclightHelper
     breadcrumb_links << add_parent_campus_link(document) unless document.parent_campus.nil? && !document.campus.nil?
     breadcrumb_links << build_repository_link(document)
     breadcrumb_links << document_parents(document).map do |parent|
-      link_to parent.label, solr_document_path(parent.global_id)
+      if parent.level =~ /Collection/i
+        link_to "Collection: #{document.collection_unitid}: #{parent.label}", solr_document_path(parent.global_id)
+      else
+        link_to parent.label, solr_document_path(parent.global_id)
+      end
     end
     safe_join(breadcrumb_links, aria_hidden_breadcrumb_separator)
   end
