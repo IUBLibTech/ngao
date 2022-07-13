96b4b5d04bb81b4af6350cfbad8f833bbabbc491
Norm Orstad <norman@orstad.com>
2022-07-13 15:13:43 -0500

    Moves metadata into 'Collection Context' tab

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -14,4 +14,15 @@ class SearchBuilder < Blacklight::SearchBuilder
   #   def add_custom_data_to_query(solr_parameters)
   #     solr_parameters[:custom] = blacklight_params[:user_value]
   #   end
+
+  self.default_processor_chain += [:return_all_fields_in_collection_context]
+
+  ##
+  # For the collection_context views, return all stored fields
+  def return_all_fields_in_collection_context(solr_params)
+    if %w[collection_context].include? blacklight_params[:view]
+      solr_params[:fl] = '*'
+    end
+    solr_params
+  end
 end
