5b8ac4fcacb1d1016cebdac5715f5a68a7de4972
Bri McLaughlin <33103290+brijmcla@users.noreply.github.com>
2024-09-03 16:34:13 -0400

    Update campus_helper.rb

--- app/helpers/campus_helper.rb
+++ app/helpers/campus_helper.rb
@@ -9,7 +9,7 @@ module CampusHelper
     'US-InU-Se' => 'Indiana University Southeast',
     'US-inrmiue' => 'Indiana University East',
     'US-InU-K' => 'Indiana University Kokomo',
-    'US-iniu' => 'IUI',
+    'US-iniu' => 'IU Indianapolis',
     'US-InU-N' => 'Indiana University Northwest'
   }
 
